6b373e3ebb47aa4492573048eeadaa7f7a06dfc5
giovanafurlan <giovananelofurlan@gmail.com>
2022-11-28 10:33:43 -0300

    Alterações

--- framework/pages/posts/api/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextApiHandler } from 'next';
-
-const handler: NextApiHandler = (req, res) => {
-  res.status(200).json({ curso: 'next.js', instrutor: 'Dev soutinho' });
-};
-
-export default handler;
